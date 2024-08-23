f71b7f9a6ad7bcedb5471260548255a53730e673
Rukundo Aime <rukundoaime78@gmail.com>
2024-08-23 12:24:27 +0200

    fix

--- app/api/job/route.ts
+++ app/api/job/route.ts
@@ -3,15 +3,15 @@ import { Job } from " @/model/job.model";
 
 export async function GET() {
   try {
-    await dbConnect();
-    const job = await Job.find({});
+    // await dbConnect();
+    // const job = await Job.find({});
 
     return Response.json({
-      data: job,
+      data: [],
       message: "success",
       status: 200,
     });
   } catch (error) {
-    throw new Error('something went wrong!')
+    throw new Error("something went wrong!");
   }
 }
