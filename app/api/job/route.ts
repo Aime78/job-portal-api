af04ae4d590299bcc4e46061497bd559a7daea66
Rukundo Aime <rukundoaime78@gmail.com>
2024-08-23 12:29:16 +0200

    fix

--- app/api/job/route.ts
+++ app/api/job/route.ts
@@ -3,7 +3,7 @@ import { Job } from " @/model/job.model";
 
 export async function GET() {
   try {
-    // await dbConnect();
+    await dbConnect();
     // const job = await Job.find({});
 
     return Response.json({
